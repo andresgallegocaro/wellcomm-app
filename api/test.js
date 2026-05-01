a5d458d0a4d792ae7b612a726bdff7cc948b6afe
andresgallegocaro <andres.gallego.caro@gmail.com>
2026-05-01 20:13:45 +0200

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   try {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(200).json({
